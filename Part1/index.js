14af5c0438390d8682cce30fea8988430c43a2af
Mark Carey <markallencarey@gmail.com>
2020-11-11 16:49:55 -0700

    finished black diamond

--- Part1/index.js
+++ Part1/index.js
@@ -1,72 +1,110 @@
 console.log('Hello world')
 
-const board = []
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-function play(clickedId) {
-  const playerSpan = document.getElementById('player')
-  const clickedElement = document.getElementById(clickedId)
+let board = []
 
-  if (playerSpan.innerText === 'X') {
-    playerSpan.innerText = 'O'
-    clickedElement.innerText = 'X'
-    board[clickedId] = 'X'
-  } else {
-    playerSpan.innerText = 'X'
-    clickedElement.innerText = 'O'
-    board[clickedId] = 'O'
-  }
+const playerSpan = document.getElementById('player')
 
-  const topLeft = board[0]
-  const topCenter = board[1]
-  const topRight = board[2]
-  const middleLeft = board[3]
-  const middleCenter = board[4]
-  const middleRight = board[5]
-  const bottomLeft = board[6]
-  const bottomCenter = board[7]
-  const bottomRight = board[8]
-
-  if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
-    window.alert(`Player ${topLeft} Won!`)
-    return
-  }
-  if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
-    window.alert(`Player ${middleLeft} Won!`)
-    return
-  }
-  if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
-    window.alert(`Player ${bottomLeft} Won!`)
-    return
-  }
-  if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
-    window.alert(`Player ${topLeft} Won!`)
-    return
-  }
-  if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
-    window.alert(`Player ${topCenter} Won!`)
-    return
-  }
-  if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
-    window.alert(`Player ${topRight} Won!`)
-    return
-  }
-  if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
-    window.alert(`Player ${topLeft} Won!`)
-    return
-  }
-  if (topRight !== undefined && topRight === middleCenter && topRight === bottomLeft) {
-    window.alert(`Player ${topRight} Won!`)
-    return
-  }
-
-  let boardFull = true
+function reset() {
+  board = []
+  playerSpan.innerText = 'X'
 
   for (let i = 0; i < 9; i++) {
-    if (board[i] === undefined) {
-      boardFull = false
-    }
-  }
-  if (boardFull === true) {
-    window.alert("CAT's game :/")
+    const iSpace = document.getElementById(i)
+    iSpace.innerText = ''
   }
 }
+
+async function play(clickedId) {
+
+  const clickedElement = document.getElementById(clickedId)
+
+  if (clickedElement.innerText === '') {
+    if (playerSpan.innerText === 'X') {
+      playerSpan.innerText = 'O'
+      clickedElement.innerText = 'X'
+      board[clickedId] = 'X'
+    } else {
+      playerSpan.innerText = 'X'
+      clickedElement.innerText = 'O'
+      board[clickedId] = 'O'
+    }
+
+    const topLeft = board[0]
+    const topCenter = board[1]
+    const topRight = board[2]
+    const middleLeft = board[3]
+    const middleCenter = board[4]
+    const middleRight = board[5]
+    const bottomLeft = board[6]
+    const bottomCenter = board[7]
+    const bottomRight = board[8]
+
+
+    if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+      await sleep(250)
+      window.alert(`Player ${topLeft} Won!`)
+      reset()
+      return
+    }
+    if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
+      await sleep(250)
+      window.alert(`Player ${middleLeft} Won!`)
+      reset()
+      return
+    }
+    if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
+      await sleep(250)
+      window.alert(`Player ${bottomLeft} Won!`)
+      reset()
+      return
+    }
+    if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
+      await sleep(250)
+      window.alert(`Player ${topLeft} Won!`)
+      reset()
+      return
+    }
+    if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
+      await sleep(250)
+      window.alert(`Player ${topCenter} Won!`)
+      reset()
+      return
+    }
+    if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
+      await sleep(250)
+      window.alert(`Player ${topRight} Won!`)
+      reset()
+      return
+    }
+    if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+      await sleep(250)
+      window.alert(`Player ${topLeft} Won!`)
+      reset()
+      return
+    }
+    if (topRight !== undefined && topRight === middleCenter && topRight === bottomLeft) {
+      await sleep(250)
+      window.alert(`Player ${topRight} Won!`)
+      reset()
+      return
+    }
+
+    let boardFull = true
+
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === undefined) {
+        boardFull = false
+      }
+    }
+    if (boardFull === true) {
+      await sleep(250)
+      window.alert("CAT's game :/")
+      reset()
+    }
+  }
+}
+
+
+
